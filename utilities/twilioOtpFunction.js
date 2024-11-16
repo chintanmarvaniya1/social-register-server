637ff888e54e9ce9c444ce621fd90ce287dfb5e5
chintanmarvaniya1 <chintanmarvaniya985@gmail.com>
2024-11-16 20:38:13 +0530

    hiding phone number

--- utilities/twilioOtpFunction.js
+++ utilities/twilioOtpFunction.js
@@ -4,7 +4,7 @@ async function sendOtp(mobile, otp) {
     try {
         await client.messages.create({
             body: `Your OTP is ${otp}`,
-            from: '+1 256 617 5452',
+            from: '',
             to: mobile
         });
         console.log(`OTP ${otp} sent to ${mobile}`);
