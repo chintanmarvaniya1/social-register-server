37df7d7cf53e8936a54e928808d0d8cdac448263
chintanmarvaniya1 <chintanmarvaniya985@gmail.com>
2024-11-17 22:03:41 +0530

    Adding Family and Member details for HOF Member

---
+++ service/familyServices/addfamily.service.js
@@ -0,0 +1,52 @@
+const FAMILY = require("../../models/family.model.js")
+const addFamily = async (id) => {
+    const familyRecord = await FAMILY.findOne({ HOF: id });
+    if (familyRecord) {
+        return {
+            status: 400,
+            message: 'USER is Already HOF of One Family'
+        };
+    }
+    const newFamily = new FAMILY({
+        HOF: id,
+        MEMBERLIST: [id]
+    });
+    await newFamily.save();
+
+    if (newFamily) {
+        return {
+            status: 200,
+            userID: UpdatedFamily.id,
+            message: 'User Registerd successfully'
+        };
+
+    }else {
+        return {
+            status: 500,
+            message: 'Failed to Register Family Contact Admin'
+        };
+    }
+
+    // if (newFamily) {
+    //     const UpdatedFamily = await MEMBER.findByIdAndUpdate(newUser.id, { FAMILY_ID: newFamily.id }, { new: true });
+    //     if (UpdatedFamily) {
+    //         return {
+    //             status: 200,
+    //             userID:UpdatedFamily.
+    //             message: 'User Registerd successfully'
+    //         };
+    //     } else {
+    //         return {
+    //             status: 500,
+    //             message: 'Failed to Register Family Contact Admin'
+    //         };
+    //     }
+    // } else {
+    //     return {
+    //         status: 500,
+    //         message: 'Failed to Register Family Contact Admin'
+    //     };
+    // }
+}
+
+module.exports = addFamily;
--- service/memberServices/addmember.service.js
+++ service/memberServices/addmember.service.js
@@ -1,4 +1,9 @@
 const MEMBER = require("../../models/member.model.js")
+const FAMILY = require("../../models/family.model.js")
+
+const addFamily = require("../familyServices/addfamily.service.js");
+const updateMember = require("../memberServices/updatemember.service.js") 
+
 const addMember = async (body) => {
     let missingFields;
     let requiredFields;
@@ -11,7 +16,7 @@ const addMember = async (body) => {
             SURNAME: body.surname,
             PHOTO: body.photo,
             RELATION: "self",
-            MOBILE_NO: body.mobile,            
+            MOBILE_NO: body.mobile,
         }
 
         if (missingFields.length > 0) {
@@ -19,43 +24,52 @@ const addMember = async (body) => {
                 status: 400,
                 message: `Invalid Input. Missing fields: ${missingFields.join(', ')}`
             };
-        }    
-        const memberRecord = await MEMBER.findOne({ MOBILE_NO:body.mobile });
+        }
+        const memberRecord = await MEMBER.findOne({ MOBILE_NO: body.mobile });
         if (memberRecord) {
             return {
                 status: 400,
                 message: 'User Already Exist with given Mobile Number'
             };
         }
-    
+
         const newUser = new MEMBER(DBObject);
         await newUser.save();
-    
         if (newUser) {
-
-            /*
-                Need to make entry in the Family DB
-                get the Object ID 
-
-                Update the current User
-            */
-            return {
-                status: 200,
-                message: 'User Registerd successfully'
-            }; 
+            const addFamilyResponse = await addFamily(newUser.id);
+            if (addFamilyResponse.status === 200) {
+                const updateMemberResponse = await updateMember(newUser.id,addFamilyResponse.userID);
+                if(updateMemberResponse.status === 200){
+                    return {
+                        status:updateMemberResponse.status,
+                        Message:`User & Family Created Successfully`,
+                        userID:newUser.id,
+                    }
+                }else{
+                    return{
+                        status: addFamilyResponse.status,
+                        message:addFamilyResponse.message
+                    }
+                }
+            } else {
+                return{
+                    status: addFamilyResponse.status,
+                    message:addFamilyResponse.message
+                }
+            }           
         } else {
             return {
                 status: 500,
                 message: 'Failed to Register User'
             };
         }
-    }else{
+    } else {
         /*
             enter the details and also update the HOF details
         */
     }
-    
-    
+
+
 
     return {
         status: 500,
